cfdd000f908a27569bf98fa819045b8326a726be
santino majur <demalong22@gmail.com>
2023-01-02 13:00:14 +0300

    Responsive navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: {
-    //     DEFAULT: '1rem',
-    //     sm: '2rem',
-    //     lg: '4rem',
-    //     xl: '5rem',
-    //     '2xl': '6rem',
-    //   },
-    // },
-    extend: {},
+    container: {
+      center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          md: '3rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        screens: {
+          md: "48rem",
+          lg: "64rem",
+          xl: "70.25rem",
+          "2xl": "70.25rem",
+        },
+    },
+    extend: {
+      fontFamily: {
+        Caramel: 'Caramel'
+      }
+    },
   },
   plugins: [],
 }
