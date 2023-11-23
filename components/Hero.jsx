89a57e0974054601f402c7addc0d31d801eead09
santino majur <demalong22@gmail.com>
2023-11-23 14:02:57 +0200

    hero component

---
+++ components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div className="">
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            Let's build something together
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#5651e5]">Santino Majur</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Full-Stack Developer </h1>
+          <p className="py-4 text-gray-600 max-w-[70%] mx-auto">
+            I'm a full-stack developer specializing in building [and
+            occasionally designing] exceptional digital experiences. Currently,
+            I'm focused on building responsive web applications.
+          </p>
+          <div className="flex justify-between items-center max-w-[338px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsPersonLinesFill />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaInstagram />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Navbar />
+      <Hero />
     </div>
   )
 }
