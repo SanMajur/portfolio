0baf112e242b3029058c9b5739a8f7db02deec59
santino majur <demalong22@gmail.com>
2023-11-30 15:06:56 +0200

    finished version

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,7 +9,7 @@ import ProjectItems from "../components/ProjectItems";
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full">
+    <div id="projects" className="w-full h-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
